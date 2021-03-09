9ae364d7c75e3c0a187e0703d998f11f07e5dc97
Mateusz Sedzimierz <mateuszsiedlce@gmail.com>
2021-03-09 23:44:39 +0100

    Add buttons to select game options

--- game.js
+++ game.js
@@ -1,20 +1,86 @@
 const SIZE = 58;
 
-$(function() {
-    let $board = $('#board');
-    for (i = 0; i < SIZE; i++) {
-        let $row = $('<tr>');
-        for (j = 0; j < SIZE; j++) {
-            $row.append(createCell(i, j));
-        }
-        $board.append($row);
-        console.log("Appended");
-    }    
+const toSurviveParams = new Array();
+const toRebornParams = new Array();
 
-    function createCell(row, column) {
-        let cell = document.createElement("td");
-        cell.id = "Cell_" + row + "_" + column;
-        cell.className = "cell";
-        return cell;
-    } 
-});
+const cells = new Array(SIZE);
+
+class Cell {
+    constructor(id, isAlive) {
+        this.id = id;
+        this.isAlive = isAlive;
+    }
+
+    setAlive(alive) {
+        const $cell = $('#' + this.id);
+        
+        if (alive === true) {
+            $cell.addClass('alive-cell');
+        } else {
+            $cell.removeClass('alive-cell');
+        }
+
+        this.isAlive = alive;
+    }
+}
+
+$(function() {
+    createOptions();
+    createBoard();
+})
+
+function createOptions() {
+    $toSurviveDiv = $('#to-survive');
+    $toRebornDiv = $('#to-reborn');
+
+    for (let i = 0; i < 9; i++) {
+      createNumberButton($toSurviveDiv, i, toSurviveParams);
+      createNumberButton($toRebornDiv, i, toRebornParams);  
+    }
+}
+
+function createNumberButton($parent, value, array) {
+    const $button = $('<button class="number-button">' + value + '</button>');
+
+    $button.on('click', function() {
+        if ($(this).hasClass('number-button-clicked')) {
+            $(this).removeClass('number-button-clicked');
+            removeFromArray(array, value);
+        } else {
+            $(this).addClass('number-button-clicked');
+            array.push(value);
+        }
+    });
+    
+    $parent.append($button);
+}
+
+function removeFromArray(array, value) {
+    const index = array.indexOf(value);
+    if (index > -1) {
+        array.splice(index, 1);
+    }
+}
+
+function createBoard() {
+    const $board = $('#board');
+
+    for (let i = 0; i < SIZE; i++) {
+        const $row = $('<tr>');
+        cells[i] = new Array(SIZE);
+        
+        for (let j = 0; j < SIZE; j++) {
+            const id = 'Cell-' + i + '-' + j;
+            $cell = $('<td id="' + id + '" class="cell">');
+            cells[i][j] = new Cell(id, false);
+ 
+            $cell.on('click', function() {
+                cells[i][j].setAlive(!cells[i][j].isAlive);
+            });
+ 
+            $row.append($cell);
+        }
+
+        $board.append($row);
+    }   
+}
