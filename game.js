d84ce095d56f736e41834c8b193be8751d96c0fc
Mateusz Sedzimierz <mateuszsiedlce@gmail.com>
2021-03-11 00:20:06 +0100

    Create basic game functionalities

--- game.js
+++ game.js
@@ -4,7 +4,9 @@ const toSurviveParams = new Array();
 const toRebornParams = new Array();
 
 const cells = new Array(SIZE);
-var aliveCells = 0;
+const nextGeneration = new Array(SIZE);
+
+var running = null;
 
 class Cell {
     constructor(id, isAlive) {
@@ -17,10 +19,8 @@ class Cell {
         
         if (alive === true) {
             $cell.addClass('alive-cell');
-            aliveCells++;
         } else {
             $cell.removeClass('alive-cell');
-            aliveCells--;
         }
 
         this.isAlive = alive;
@@ -31,9 +31,19 @@ $(function() {
     createOptions();
     createBoard();
 
-    $('#start').on('click', function() {
-        startIsPossible();
+    $('#start').on('click', function(event) {
+        if (running == null) {
+            start();
+        } else {
+            stop();
+        }
     });
+
+    $('#next-step').on('click', function() {
+        if (startIsPossible()) {
+            makeStep();
+        }
+    })
 
     $('#clear').on('click', () => clearBoard());
 
@@ -74,19 +84,21 @@ function removeFromArray(array, value) {
 }
 
 function createBoard() {
-    const $board = $('#board');
+    let $board = $('#board');
 
     for (let i = 0; i < SIZE; i++) {
-        const $row = $('<tr>');
+        let $row = $('<tr>');
         cells[i] = new Array(SIZE);
-        
+        nextGeneration[i] = new Array(SIZE);
+                
         for (let j = 0; j < SIZE; j++) {
-            const id = 'Cell-' + i + '-' + j;
+            let id = 'Cell-' + i + '-' + j;
             $cell = $('<td id="' + id + '" class="cell">');
             cells[i][j] = new Cell(id, false);
  
             $cell.on('click', function() {
-                cells[i][j].setAlive(!cells[i][j].isAlive);
+                if (running === null)
+                    cells[i][j].setAlive(!cells[i][j].isAlive);
             });
  
             $row.append($cell);
@@ -97,13 +109,13 @@ function createBoard() {
 }
 
 function startIsPossible() {
-    const $message = $('#error-message');
+    let $message = $('#error-message');
     
     if (toSurviveParams.length == 0 || toRebornParams.length == 0) {
         $message
             .text('Parameters cannot be empty')
             .fadeTo(1000, 1);
-    } else if (aliveCells == 0) {
+    } else if ($('.alive-cell').length == 0) {
         $message
             .text('Select any alive cells')
             .fadeTo(1000, 1)
@@ -115,6 +127,85 @@ function startIsPossible() {
     return false;
 }
 
+function start() {
+    if (startIsPossible()) {
+        $('#start').text('Stop');
+        setButtonsDisabled(true);
+        running = setInterval(makeStep, 1000);
+    }
+}
+
+function stop() {
+    if (running !== null) {
+        $('#start').text('Start');
+        setButtonsDisabled(false);
+        clearInterval(running);
+        running = null;
+    }
+}
+
+function setButtonsDisabled(disabled) {
+    $('#next-step').prop('disabled', disabled);
+    $('#clear').prop('disabled', disabled);
+    $('#random').prop('disabled', disabled);
+    $('.number-button').prop('disabled', disabled);
+    $('.cell').css('cursor', disabled === true ? 'default' : 'pointer');
+}
+
+function makeStep() {
+    for (let i = 0; i < SIZE; i++) {
+        for (let j = 0; j < SIZE; j++) {
+            let neighbours = countLivingNeighbours(i, j);
+            nextGeneration[i][j] = false;
+            if (cells[i][j].isAlive === true) {
+                if (toSurviveParams.includes(neighbours)) {
+                    nextGeneration[i][j] = true;
+                }
+            } else if (toRebornParams.includes(neighbours)) {
+                nextGeneration[i][j] = true;
+            }
+        }
+    }
+    updateCells();
+}
+
+function countLivingNeighbours(row, column) {
+    let counter = 0;
+    counter += testNeighbour(row - 1, column - 1);
+    counter += testNeighbour(row - 1, column);
+    counter += testNeighbour(row - 1, column + 1);
+    counter += testNeighbour(row, column - 1);
+    counter += testNeighbour(row, column + 1);
+    counter += testNeighbour(row + 1, column - 1);
+    counter += testNeighbour(row + 1, column);
+    counter += testNeighbour(row + 1, column + 1);
+    return counter;
+}
+
+function testNeighbour(x, y) {
+    try {
+        if (cells[x][y].isAlive === true) {
+            return 1;
+        }
+    } catch (error) {}
+    return 0;
+}
+
+function updateCells() {
+    let updated = 0;
+    for (let i = 0; i < SIZE; i++) {
+        for (let j = 0; j < SIZE; j++) {
+            let state = nextGeneration[i][j];
+            if (cells[i][j].isAlive !== state) {
+                cells[i][j].setAlive(state);
+                updated++;
+            }
+        }
+    }
+    if (updated === 0) {
+        stop();
+    }
+}
 
 function clearBoard() {
     for (let i = 0; i < SIZE; i++) {
